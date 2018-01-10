2e456572672fca3ae1da5af42b2860b5b28841c7
Isaias Arellano <iarellano@nearbpo.com>
2018-01-10 00:16:31 -0500

    Fix JS

--- apiproxy/resources/jsc/MashUpIpAndUuid.js
+++ apiproxy/resources/jsc/MashUpIpAndUuid.js
@@ -1,10 +1,12 @@
- var orginObj=JSON.parse(context.getVariable("response.content"));
+/* globals context */
+
+var orginObj=JSON.parse(context.getVariable("response.content"));
  var uuidObj=JSON.parse(context.getVariable("uuidResponse.content"));
  
  var response={
      "ip": orginObj.origin,
      "uuid": uuidObj.uuid
- }
+ };
  
  context.setVariable('response.content', JSON.stringify(response));
  context.setVariable('response.header.X-New-Relic-Id', uuidObj.uuid);
