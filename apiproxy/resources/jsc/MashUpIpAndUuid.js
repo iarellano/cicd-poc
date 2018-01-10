a7acbe520cec46442540670eeaab39e37b477d4b
Isaias Arellano <iarellano@nearbpo.com>
2018-01-09 19:36:39 -0500

    Initial commit

---
+++ apiproxy/resources/jsc/MashUpIpAndUuid.js
@@ -0,0 +1,10 @@
+ var orginObj=JSON.parse(context.getVariable("response.content"));
+ var uuidObj=JSON.parse(context.getVariable("uuidResponse.content"));
+ 
+ var response={
+     "ip": orginObj.origin,
+     "uuid": uuidObj.uuid
+ }
+ 
+ context.setVariable('response.content', JSON.stringify(response));
+ context.setVariable('response.header.X-New-Relic-Id', uuidObj.uuid);
